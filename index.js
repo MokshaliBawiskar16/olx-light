e19d6367efb6a631c74529ab83952d82cc7ac7f0
mokshali <mokshalibawiskar16@gmail.com>
2025-03-03 16:55:51 +0530

    log

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cors=require("cors")
 require("dotenv").config()
 const cookieParser=require("cookie-parser")
 const { adminProtected, customerProtected } = require("./middleware/protected")
+const path=require("path")
 
 const app=express()
 app.use(express.json())//req.body
 app.use(cookieParser())//req.cookies
+app.use(express.static("dist"))//req.cookies
 
 // app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cors({origin:true,credentials:true}))
@@ -18,11 +20,11 @@ app.use("/api/public",require("./routes/public.routes"))
 app.use("/api/customer",customerProtected,require("./routes/customer.routes"))
 
 
-
-
-app.use("*",(req,res)=>{
-    res.status(404).json({message:"resourse not found"})
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
+
+
 app.use((err,req,res,next)=>{
     console.log(err);
     res.status(500).json({message:"server error"})
