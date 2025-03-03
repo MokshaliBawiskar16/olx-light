fe91fdffaabe27a7e8f790f6b0866a24b3027624
mokshali <mokshalibawiskar16@gmail.com>
2025-03-03 19:51:58 +0530

    cors

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json())//req.body
 app.use(cookieParser())//req.cookies
 app.use(express.static("dist"))//req.cookies
 
-// app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(cors({origin:true,credentials:true}))
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
+// app.use(cors({origin:true,credentials:true}))
 app.use("/api/auth",require("./routes/auth.routes"))
 app.use("/api/product",customerProtected,require("./routes/product.routes"))
 app.use("/api/admin",adminProtected,require("./routes/admin.routes"))
@@ -32,6 +32,6 @@ app.use((err,req,res,next)=>{
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection.once("open",()=>{
     console.log("db connected");
-    app.listen(process.env.PORT || 5000 ,console.log("server runnings")
+    app.listen(process.env.PORT ,console.log("server runnings")
     )
 })
